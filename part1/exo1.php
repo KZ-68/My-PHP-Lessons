f5df42e418d52ea6ad795047217c2abd03fd0c3b
KZ-68 <127909706+KZ-68@users.noreply.github.com>
2023-03-23 08:16:38 +0100

    Update and rename exo1.php to part1/exo1.php

--- part1/exo1.php
+++ part1/exo1.php
@@ -11,3 +11,5 @@ $phrase = "Notre formation DL commence aujourd'hui";
 $longueur = strlen($phrase);
 echo "La phrase  $phrase  contient $longueur caractères<br>";
 echo "La phrase  $phrase  contient " .strlen($phrase). " caractères<br>";
+
+?>
